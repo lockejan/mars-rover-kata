6a66c86638ad926b647d4f9d60f593de0aa62786
Jan Schmitt <jan.schmitt@hhu.de>
2019-02-16 11:31:05 +0100

    extended docstrings. made all internal fn's private. changed error message handling.

--- cruisen/src/cruisen/core.clj
+++ cruisen/src/cruisen/core.clj
@@ -3,19 +3,19 @@
 
 (def state (atom {}))
 
-(defn pos [x y]
-  "expects two lonely coordinates and brings them into a map."
+(defn- pos [x y]
+  "Expects two lonely coordinates and brings them into a map."
   {:x x, :y y})
 
-(defn get-obstacle-positions [height width amount-of-obstacles]
-  "randomly places the already defined obstacles on the board."
+(defn- get-obstacle-positions [height width amount-of-obstacles]
+  "Randomly places the already defined obstacles on the board."
   (let [entire-board (for [x (range width)
                            y (range height)]
                        (pos x y))]
     (take amount-of-obstacles (shuffle (next entire-board)))))
 
-(defn pretty-print [value]
-  "maps a given value to representative chars."
+(defn- pretty-print [value]
+  "Maps a given value to representative chars."
   (case value
     :N \∧
     :S \∨
@@ -26,7 +26,8 @@
     value))
 
 (defn print-board! []
-  "prints board state to console."
+  "Prints board state to console."
+  "Should be called without without parameters."
   (let [{:keys [height width obstacles rover]} @state]
     (doseq [y (range height)]
       (doseq [x (range width)]
@@ -36,15 +37,15 @@
           (print (pretty-print value))))
       (newline))))
 
-(defn collision! [state tmp-step]
-  "checks if tmp-step collides with obstacle and prints the position of the obstacle."
+(defn- collision [state tmp-step]
+  "Checks if tmp-step collides with obstacle and prints the position of the obstacle."
   (let [obstacles (get-in state [:obstacles])]
     (if (contains? obstacles tmp-step)
-      (do (println (format "obstacle ahead at: %s" tmp-step)) state)
+      (assoc-in state [:status] {:error (format "Obstacle ahead at: %s" tmp-step)})
       (assoc-in state [:rover :position] tmp-step))))
 
-(defn tmp-move [state step]
-  "calculates tmp-move without considering board borders."
+(defn- tmp-move [state step]
+  "Calculates tmp-move without considering board borders."
   (let [rover-x (get-in state [:rover :position :x])
         rover-y (get-in state [:rover :position :y])]
     (case (get-in state [:rover :direction])
@@ -53,8 +54,8 @@
       :S (pos rover-x (+ rover-y step))
       :W (pos (- rover-x step) rover-y))))
 
-(defn wrap-move [new-pos state]
-  "handles wrap around of rover."
+(defn- wrap-move [new-pos state]
+  "Handles wrap around of rover."
   (let [height (get-in state [:height])
         width (get-in state [:width])]
     (cond
@@ -64,70 +65,93 @@
       (> (new-pos :x) (dec width)) (update new-pos :x - width)
       :else new-pos)))
 
-(defn move [state step]
-  "chaining all necessary operations. conditionals are handled inside the functions who is in charge of the current operation."
-  (collision! state (wrap-move (tmp-move state step) state)))
+(defn- move [state step]
+  "Chaining all necessary operations to move the rover.
+  Conditionals are handled inside the functions which is in charge of the current operation."
+  (collision state (wrap-move (tmp-move state step) state)))
 
-(defn new-d [state d]
-  "update direction of rover."
-  (assoc-in state [:rover :direction] d))
+(defn- new-d [state new-d]
+  "Update direction of rover."
+  (assoc-in state [:rover :direction] new-d))
 
-(defn new-r-wrap [r-idx]
-  "correct index to be valid inside direction set."
+(defn- new-r-wrap [r-idx]
+  "Correct index to be valid inside direction set."
   (cond
     (< r-idx 0) (+ r-idx 4)
     (> r-idx 3) (- r-idx 4)
     :else r-idx))
 
-(defn indexes-of [e coll]
-  "returns index of first occurrence of e in collection."
+(defn- indexes-of [e coll]
+  "Returns index of first occurrence of elem in collection."
   (first (keep-indexed #(if (= e %2) %1) coll)))
 
-(defn rotate [state x]
-  "calculates and handles direction update of rover."
+(defn- rotate [state x]
+  "Calculates and handles direction update of rover."
   (let [r-map (list :N :E :S :W)]
     (case (get-in state [:rover :direction])
-             :N (new-d state (nth r-map (new-r-wrap (+ (indexes-of :N r-map) x))))
-             :E (new-d state (nth r-map (new-r-wrap (+ (indexes-of :E r-map) x))))
-             :S (new-d state (nth r-map (new-r-wrap (+ (indexes-of :S r-map) x))))
-             :W (new-d state (nth r-map (new-r-wrap (+ (indexes-of :W r-map) x)))))))
+      :N (new-d state (nth r-map (new-r-wrap (+ (indexes-of :N r-map) x))))
+      :E (new-d state (nth r-map (new-r-wrap (+ (indexes-of :E r-map) x))))
+      :S (new-d state (nth r-map (new-r-wrap (+ (indexes-of :S r-map) x))))
+      :W (new-d state (nth r-map (new-r-wrap (+ (indexes-of :W r-map) x)))))))
 
 (defn init!
-  "initial board creation.
-  default values are applied if no values are provided."
-  ([] (init! 10 20 (* 10 20 0.1)))
-  ([height width amount-of-obstacles]
+  "Initial board creation.
+  Accepts two integer values to define the board size.
+    Example call: (init! 10 20)
+  Default values are applied if no values are provided."
+  ([] (init! 10 20))
+  ([height width]
    (reset! state {:height    height
                   :width     width
+                  :status    {:fine "Nothing suspicious"}
                   :rover     {:position {:x 0 :y 0} :direction :N}
                   :obstacles (set (get-obstacle-positions height
                                                           width
-                                                          amount-of-obstacles))})
+                                                          (* height width 0.1)))})
    (print-board!)))
 
-(defn no-match! [state x]
-"handles feedback in case of invalid input characters."
-  (do (println (format "No matching case implemented for \"%s\"...yet. \nUse l,r,f or b instead." x)) state))
+(defn- no-match [state x]
+  "Handles feedback in case of invalid input characters."
+  (assoc-in state [:status] {:error (format "No matching case implemented for \"%s\"...yet. \nUse l,r,f or b instead." x)}))
 
 (defmulti hello (fn [_ x] x))
 (defmethod hello \f [state _] (move state 1))
 (defmethod hello \b [state _] (move state -1))
 (defmethod hello \l [state _] (rotate state -1))
 (defmethod hello \r [state _] (rotate state 1))
-(defmethod hello :default [state x] (no-match! state x))
+(defmethod hello :default [state x] (no-match state x))
 
-(defn hello-rover [state msg]
-  "walks through the cmd sequence for the rover."
+(defn- check-error [state msg]
+  (if (contains? (get-in state [:status]) :error)
+    nil
+    msg))
+
+(defn- print-error! []
+  (let [{:keys [status]} @state]
+    (if (contains? status :error)
+      (println (get status :error))
+      nil)))
+
+(defn- hello-rover [state msg]
+  "Walks through the cmd sequence for the rover, char by char."
   (if (empty? msg)
     state
     (let [new-state (hello state (first msg))]
-      (recur new-state (rest msg)))))
+      (recur new-state (rest (check-error state msg))))))
 
 (defn hello-rover! [msg]
-  "gets the state and prints the board after all operations are done."
+  "Accepts a character sequence.
+  Valid chars are \"l, \"r, \"f and \"b.
+  \"f moves the rover forwards, \"b moves it backwards.
+  \"l let's it rotate counter-clockwise \"b clockwise.
+  Gets the state and prints the board after all operations are done."
   (swap! state hello-rover msg)
-  (print-board!))
+  (print-board!)
+  (print-error!)
+  (let [{:keys [status]} @state]
+    (if (contains? status :error)
+      (do (swap! state assoc :status {:fine "Nothing suspicious"}) nil))))
 
 ; Init and first test
-;(init!)
-;(hello-rover! "fffxf")
+(init!)
+;(hello-rover! "fff")
